20b9b82cc42120b826b9e528782ca53b6abc5c39
Robin <1105080+openscript@users.noreply.github.com>
2020-04-19 19:06:28 +0200

    Enhance context tests

--- src/features/context.test.ts
+++ src/features/context.test.ts
@@ -1,18 +1,26 @@
 import { reducer, getDSVImportContext } from './context';
 import { State } from '../models/state';
+import { useContext } from 'react';
+import { renderHook } from '@testing-library/react-hooks';
 
 describe('context', () => {
   type TestType = {};
   const defaultState: State<TestType> = { columns: [] };
 
-  it('should reduce raw data to state', () => {
+  it('should reduce raw data action to state', () => {
     const newState = reducer<TestType>(defaultState, { type: 'setRaw', raw: 'Raw data' });
     expect(newState).toStrictEqual({ ...defaultState, raw: 'Raw data' });
   });
 
-  it('should reduce parsed data to state', () => {
+  it('should reduce parsed data action to state', () => {
     const newState = reducer<TestType>(defaultState, { type: 'setParsed', parsed: ['Parsed data'] });
     expect(newState).toStrictEqual({ ...defaultState, parsed: ['Parsed data'] });
+  });
+
+  it('should return state on unknown action', () => {
+    const untypedReducer = reducer as <T>(state: State<T>, action: unknown) => State<T>;
+    const newState = untypedReducer<TestType>(defaultState, { type: 'unknown' });
+    expect(newState).toStrictEqual(defaultState);
   });
 
   it('should create the context as singleton', () => {
@@ -20,5 +28,17 @@ describe('context', () => {
     const second = getDSVImportContext<TestType>();
 
     expect(first).toStrictEqual(second);
+  });
+
+  it('should throw an error when the context is not initialized', () => {
+    const context = getDSVImportContext<TestType>();
+    const { result } = renderHook(() => useContext(context));
+    const [, dispatch] = result.current;
+
+    const provokeError = () => {
+      dispatch({ type: 'setRaw', raw: 'fail' });
+    };
+
+    expect(provokeError).toThrowError('Not initialized');
   });
 });
