bebcde837ba748871926b18974ba742df42adc48
Robin <1105080+openscript@users.noreply.github.com>
2020-05-25 16:15:50 +0200

    Enhance exported types

--- src/models/column.ts
+++ src/models/column.ts
@@ -1,4 +1,5 @@
 import { Rule } from './rule';
 import { Transformer } from './transformer';
 
-export type ColumnsType<T> = { key: keyof T; label: string; rules?: Rule[]; transformers?: Transformer[] }[];
+export type ColumnType<T> = { key: keyof T; label: string; rules?: Rule[]; transformers?: Transformer[] };
+export type ColumnsType<T> = ColumnType<T>[];
