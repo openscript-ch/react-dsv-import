90a24733d66066d024fb6a634af463a63fa29607
Robin <1105080+openscript@users.noreply.github.com>
2020-05-26 09:51:59 +0200

    Export validation error type

--- src/index.tsx
+++ src/index.tsx
@@ -15,3 +15,4 @@ export { ColumnType, GenericColumnType } from './models/column';
 export { useDSVImport } from './features/context';
 export { Rule } from './models/rule';
 export { Transformer } from './models/transformer';
+export { ValidationError } from './models/validation';
