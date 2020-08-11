bff14e06491c947d690bc04561a659d7c18e08bd
Robin <1105080+openscript@users.noreply.github.com>
2020-08-11 16:04:49 +0200

    Enhance reexports

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,4 @@
-import { PropsWithChildren } from 'react';
-import { DSVImport as Import, Props } from './DSVImport';
-import { GenericColumnType } from './models/column';
-import { TextareaInput } from './components/inputs/TextareaInput';
-import { TablePreview } from './components/previews/TablePreview';
-
-export function DSVImport<T extends GenericColumnType>(props: PropsWithChildren<Props<T>>) {
-  return Import<T>(props);
-}
-
-DSVImport.TextareaInput = TextareaInput;
-DSVImport.TablePreview = TablePreview;
-
+export { DSVImport } from './DSVImport';
 export { ColumnType, GenericColumnType } from './models/column';
 export { useDSVImport } from './features/context';
 export { Rule } from './models/rule';
