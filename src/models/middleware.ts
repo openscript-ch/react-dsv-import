2f65f7c594d71225f2c88bbac1480c4d03a6bb95
Robin <1105080+openscript@users.noreply.github.com>
2020-08-19 12:26:04 +0200

    Clean up middleware

--- src/models/middleware.ts
+++ src/models/middleware.ts
@@ -1,4 +1,4 @@
-import { State } from "./state";
-import { Dispatch } from "react";
+import { State } from './state';
+import { Dispatch } from 'react';
 
 export type Middleware<T, A> = (state: State<T>, dispatch: Dispatch<A>, action: A) => void;
